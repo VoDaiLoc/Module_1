986325afa1c0e741e2725316a1544367afac291e
VoDaiLoc <vodailoc.kt@gmail.com>
2022-04-10 00:35:23 +0700

    add folder use_avariable

---
+++ JavaScript/use_variable/main.js
@@ -0,0 +1,30 @@
+// Bài tập 1:
+let i = 10
+let f = 20.5
+let b = true
+let s = 'Hà Nội'
+document.write('i = ' + i)
+document.write('<br>')
+document.write('f = ' + f)
+document.write('<br>')
+document.write('b = ' + b)
+document.write('<br>')
+document.write('s = ' + s)
+document.write('<br>')
+// Bài tập 2:
+let width = 20
+let height = 10
+let area = width*height
+document.write('area =' + area)
+document.write('<br>')
+// Bài tập 3:
+let x = prompt('Nhập vào số x: ')
+let y = prompt('Nhập vào số y: ')
+document.write('x =' + x)
+document.write('<br>')
+document.write('y =' + y)
+document.write('<br>')
+if (x%y==0) {
+    document.write('x chia hết cho y')
+} else
+    document.write('x không chia hết cho y')
