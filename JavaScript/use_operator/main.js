fcacdd46b6ab2244ef62fb267c9bf431d8b49dc4
VoDaiLoc <vodailoc.kt@gmail.com>
2022-04-10 00:45:09 +0700

    add folder use operator

---
+++ JavaScript/use_operator/main.js
@@ -0,0 +1,6 @@
+let inputWidth = prompt('Nhập vào chiều rộng:')
+let inputHeight = prompt('Nhập vào chiều cao:')
+let width = parseInt(inputWidth)
+let height = parseInt(inputHeight)
+let area = width*height
+document.write('The area is: ' + area)
